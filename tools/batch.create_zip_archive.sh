8b55b37ce91bcaa83e3eee73d7335b50eccaa98b
kei <kmikage@gmail.com>
2021-12-30 19:19:19 +0900

    fix issue/3

--- tools/batch.create_zip_archive.sh
+++ tools/batch.create_zip_archive.sh
@@ -9,5 +9,6 @@ if [ -f "${FN_PASSWD_ZIP}" ]; then
  rm ${FN_PASSWD_ZIP}
 fi
 
-#zip -9 ${FN_PASSWD_ZIP} ${FN_PASSWD}
-zip ${FN_PASSWD_ZIP} ${FN_PASSWD}
+zip -9 ${FN_PASSWD_ZIP} ${FN_PASSWD} > /dev/null 2> /dev/null
+
+
--- tools/batch.sh
+++ tools/batch.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# merge passwords
+
+./tools/batch.merge_passwords.sh 2>/dev/null
+
+./tools/batch.split_default.sh 2> /dev/null &
+./tools/batch.split_dot.sh 2> /dev/null &
+./tools/batch.split_nmc.sh 2> /dev/null &
+./tools/batch.create_zip_archive.sh 2> /dev/null &
+
+wait
+
+
