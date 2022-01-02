46f2be1f2d23efef297ba628901ed8b22132b5ca
kei <kmikage@gmail.com>
2022-01-02 14:00:54 +0900

    fix zip archive.

--- tools/batch.create_zip_archive.sh
+++ tools/batch.create_zip_archive.sh
@@ -2,8 +2,10 @@
 
 # cereate passwords.zip
 
-FN_PASSWD="./passwords/passwords.txt"
-FN_PASSWD_ZIP="./passwords/passwords.zip"
+pushd ./passwords
+
+FN_PASSWD="./passwords.txt"
+FN_PASSWD_ZIP="./passwords.zip"
 
 if [ -f "${FN_PASSWD_ZIP}" ]; then
  rm ${FN_PASSWD_ZIP}
@@ -11,4 +13,4 @@ fi
 
 zip -9 ${FN_PASSWD_ZIP} ${FN_PASSWD} > /dev/null 2> /dev/null
 
-
+popd
