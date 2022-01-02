64fe15e3b3478e0c3e3ef8666253732f90decb85
kei <kmikage@gmail.com>
2022-01-03 01:29:18 +0900

    fix gzip comp.

--- tools/batch.create_zip_archive.sh
+++ tools/batch.create_zip_archive.sh
@@ -4,9 +4,12 @@
 
 pushd ./passwords
 
-FN_PASSWD="./passwords.txt"
-FN_PASSWD_ZIP="./passwords.zip"
-FN_PASSWD_GZ="./passwords.gz"
+FN_PASSWD="passwords.txt"
+FN_PASSWD_ZIP="passwords.zip"
+FN_PASSWD_GZ="passwords.gz"
+FN_PREFIX="passwords_"
+FN_HASH="passwords.sha256"
+DN_OUTPUT="../passwords"
 
 if [ -f "${FN_PASSWD_ZIP}" ]; then
  rm ${FN_PASSWD_ZIP}
@@ -14,7 +17,11 @@ fi
 
 zip -9 ${FN_PASSWD_ZIP} ${FN_PASSWD} > /dev/null 2> /dev/null &
 cat ${FN_PASSWD} | gzip > ${FN_PASSWD_GZ} &
-
 wait
+
+HASH=`cat ${DN_OUTPUT}/${FN_PASSWD_ZIP} | sha256sum | awk -F' ' '{print $1}'`
+echo "${FN_PASSWD_ZIP} ${HASH}" >> ${DN_OUTPUT}/${FN_HASH}
+HASH=`cat ${DN_OUTPUT}/${FN_PASSWD_GZ} | sha256sum | awk -F' ' '{print $1}'`
+echo "${FN_PASSWD_GZ} ${HASH}" >> ${DN_OUTPUT}/${FN_HASH}
 
 popd
