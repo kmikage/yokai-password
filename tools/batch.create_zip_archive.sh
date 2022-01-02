14efe1033e5313e0428053a5f3a31dc71f334a12
kei <kmikage@gmail.com>
2022-01-02 23:05:19 +0900

    add gzip archive file.

--- tools/batch.create_zip_archive.sh
+++ tools/batch.create_zip_archive.sh
@@ -6,11 +6,15 @@ pushd ./passwords
 
 FN_PASSWD="./passwords.txt"
 FN_PASSWD_ZIP="./passwords.zip"
+FN_PASSWD_GZ="./passwords.gz"
 
 if [ -f "${FN_PASSWD_ZIP}" ]; then
  rm ${FN_PASSWD_ZIP}
 fi
 
-zip -9 ${FN_PASSWD_ZIP} ${FN_PASSWD} > /dev/null 2> /dev/null
+zip -9 ${FN_PASSWD_ZIP} ${FN_PASSWD} > /dev/null 2> /dev/null &
+cat ${FN_PASSWD} | gzip > ${FN_PASSWD_GZ} &
+
+wait
 
 popd
