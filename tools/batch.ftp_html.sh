b0233edc18f68ddf8eb54672a65cc9b639fb5d71
kei <kmikage@gmail.com>
2022-01-21 18:27:03 +0900

    add upload tools.

---
+++ tools/batch.ftp_html.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -e "config.ftp_html.sh" ]; then
+ . config.ftp_html.sh 
+else
+ echo "no config file."
+ exit 1
+fi
+
+DIR_WORK=$(mktemp -d)
+
+cat ${DIR_HTML}/index.utf8.html | nkf -s > ${DIR_WORK}/index.html
+cat ${DIR_HTML}/style.css | nkf -s > ${DIR_WORK}/style.css
+
+${CMD_FTP} ${CFG_FTP_SV} ${CFG_FTP_ID} ${CFG_FTP_PW} ${DIR_WORK}/index.html
+${CMD_FTP} ${CFG_FTP_SV} ${CFG_FTP_ID} ${CFG_FTP_PW} ${DIR_WORK}/style.css
+
+rm -rf ${DIR_WORK}
+
+exit 0
---
+++ tools/config.ftp_html.sh
@@ -0,0 +1,6 @@
+DIR_HTML="./website"
+CMD_FTP="./tools/ftp_put.sh"
+
+CFG_FTP_SV=""
+CFG_FTP_ID=""
+CFG_FTP_PW=""
