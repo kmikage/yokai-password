aec12cb9f53a04759a5c4a201290e0d3c0ee1625
kei <kmikage@gmail.com>
2022-01-21 18:33:52 +0900

    update batch ftp tools.

---
+++ tools/config.ftp_html.sample.sh
@@ -0,0 +1,8 @@
+# Edit and Rename 'config.ftp_html.sh'.
+
+DIR_HTML="./website"
+CMD_FTP="./tools/ftp_put.sh"
+
+CFG_FTP_SV="" # FTP Server
+CFG_FTP_ID="" # FTP ID
+CFG_FTP_PW="" # FTP Password
--- tools/config.ftp_html.sh
+++
@@ -1,6 +0,0 @@
-DIR_HTML="./website"
-CMD_FTP="./tools/ftp_put.sh"
-
-CFG_FTP_SV=""
-CFG_FTP_ID=""
-CFG_FTP_PW=""
