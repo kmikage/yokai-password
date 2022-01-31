d18f7f33eed155b466715c5ff5b5c27d22922d42
kei <kmikage@gmail.com>
2022-01-31 20:36:15 +0900

    bug fix.

--- tools/batch.rejected_passwords.merge.sh
+++ tools/batch.rejected_passwords.merge.sh
@@ -2,22 +2,11 @@
 
 # rejected_passwords_original以下のrpasswords_*.txtをマージ・sort -uする
 
-FN_TEMP=$(mktemp)
 FN_PASSWD="./rejected_passwords/rpasswords.txt"
 
-find ./rejected_passwords_original/ -name 'rpasswords_*.txt' \
- | while read FN
-   do
-    cat ${FN} >> ${FN_TEMP}
-    echo '' >> ${FN_TEMP}
-   done
-
-cat ${FN_TEMP} \
- | sort -u \
- | grep -v '^$' \
-  > ${FN_PASSWD}
-
-rm ${FN_TEMP}
+cat ./rejected_passwords_original/*/rpasswords_*.txt \
+| sort -u \
+> ${FN_PASSWD}
 
 exit 0
 
