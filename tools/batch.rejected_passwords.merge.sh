6648a90cd84512c1dfa930ce15c7837fe513fc6f
kei <kmikage@gmail.com>
2022-02-01 03:33:50 +0900

    fix merge tool.

--- tools/batch.rejected_passwords.merge.sh
+++ tools/batch.rejected_passwords.merge.sh
@@ -2,22 +2,12 @@
 
 # rejected_passwords_original以下のrpasswords_*.txtをマージ・sort -uする
 
-FN_TEMP=$(mktemp)
 FN_PASSWD="./rejected_passwords/rpasswords.txt"
+FN_PASSWD_ORIGINAL="./rejected_passwords_original/*/rpasswords_*.txt"
 
-find ./rejected_passwords_original/ -name 'rpasswords_*.txt' \
- | while read FN
-   do
-    cat ${FN} >> ${FN_TEMP}
-    echo '' >> ${FN_TEMP}
-   done
-
-cat ${FN_TEMP} \
- | sort -u \
- | grep -v '^$' \
-  > ${FN_PASSWD}
-
-rm ${FN_TEMP}
+cat ${FN_PASSWD_ORIGINAL} \
+| sort -u \
+> ${FN_PASSWD}
 
 exit 0
 
